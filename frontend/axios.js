43c90918cb1646c4bc072b5b89ca2c2a72f4dc43
Lucas <65600775+lukyyy9@users.noreply.github.com>
2024-10-15 09:58:31 +0200

    Update axios.js

--- frontend/axios.js
+++ frontend/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:4000/api/',
+    baseURL: 'http://54.162.10.55:4000/api/',
     headers: {
         'Content-Type': 'application/json'
     }
