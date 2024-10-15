2ac30d66099b9934adb1b95d6801df42264e5483
Lucas <65600775+lukyyy9@users.noreply.github.com>
2024-10-15 15:29:02 +0200

    prod&dev ready !

--- backend/lib/timemanager/repo.ex
+++ backend/lib/timemanager/repo.ex
@@ -3,13 +3,42 @@ defmodule Timemanager.Repo do
     otp_app: :timemanager,
     adapter: Ecto.Adapters.Postgres
 
+  # Fonction qui lit et parse le fichier .env
+  defp load_env_file do
+    # Spécifier le chemin de ton fichier .env
+    env_file_path = Path.expand("../../.env", __DIR__)
+
+    # Vérifie si le fichier existe avant de le lire
+    if File.exists?(env_file_path) do
+      # Lire le fichier ligne par ligne et le parser
+      env_file_path
+      |> File.read!()
+      |> String.split("\n")
+      |> Enum.reduce(%{}, fn line, acc ->
+        case String.split(line, "=") do
+          [key, value] -> Map.put(acc, key, String.trim(value))
+          _ -> acc
+        end
+      end)
+    else
+      %{}  # Retourne une map vide si le fichier n'existe pas
+    end
+  end
+
   def init(_, config) do
+    env_vars = load_env_file()
+
+    #log the env vars
+    IO.inspect(env_vars)
+
+    # Remplacer les valeurs des variables d'environnement si elles sont nulles
     config = config
-      |> Keyword.put(:username, System.get_env("PGUSER"))
-      |> Keyword.put(:password, System.get_env("PGPASSWORD"))
-      |> Keyword.put(:database, System.get_env("PGDATABASE"))
-      |> Keyword.put(:hostname, System.get_env("PGHOST"))
-      |> Keyword.put(:port, System.get_env("PGPORT") |> String.to_integer)
+    |> Keyword.put(:username, System.get_env("PGUSER") || env_vars["PGUSER"])
+    |> Keyword.put(:password, System.get_env("PGPASSWORD") || env_vars["PGPASSWORD"])
+    |> Keyword.put(:database, System.get_env("PGDATABASE") || env_vars["PGDATABASE"])
+    |> Keyword.put(:hostname, System.get_env("PGHOST") || env_vars["PGHOST"])
+    |> Keyword.put(:port, (System.get_env("PGPORT") || env_vars["PGPORT"]) |> String.to_integer)
+
     {:ok, config}
   end
 end
