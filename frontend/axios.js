02a86caf2b68bed54801feae71c2bf96ac1eef4e
Lucas <65600775+lukyyy9@users.noreply.github.com>
2024-10-15 12:06:26 +0200

    test

--- frontend/axios.js
+++ frontend/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://54.162.10.55:4000/api/',
+    baseURL: import.meta.env.VITE_AXIOSBASEURL,
     headers: {
         'Content-Type': 'application/json'
     }
