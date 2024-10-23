ec74d5bb50b157d68d007571b33aebaf4d4ddb5d
Lisa <lisa.nottoli@epitech.eu>
2024-10-23 14:31:16 +0200

    seed creation admin user

--- backend/priv/repo/seeds.exs
+++ backend/priv/repo/seeds.exs
@@ -9,3 +9,26 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias Timemanager.Repo
+alias Timemanager.UserContext.User
+alias Timemanager.UserContext
+
+
+admin_exists = Repo.get_by(User, username: "admin")
+
+
+if admin_exists do
+  IO.puts("Admin user already exists")
+else
+  %{
+    "username" => "admin",
+    "email" => "route@admin.com",
+    "password" => "route123",
+    "role" => 3
+  }
+  |> UserContext.create_user()
+  |> case do
+    {:ok, _user} -> IO.puts("Admin user created successfully")
+    {:error, reason} -> IO.puts("Failed to create admin user: #{inspect(reason)}")
+  end
+end
